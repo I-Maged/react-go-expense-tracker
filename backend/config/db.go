c98b27b21baf7e55f5d618a9cde901a43b4bdcc0
I-Maged <magedhanafy@gmail.com>
2026-05-26 04:32:40 +0300

    connect to postgres database

---
+++ backend/config/db.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"backend/models"
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+
+	var db *gorm.DB
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Could not connect to the database")
+	}
+
+	err = db.AutoMigrate(&models.User{}, &models.Transaction{})
+	if err != nil {
+		log.Fatalf("Failed to run auto-migrations: %v", err)
+	}
+
+	log.Println("Database migrations applied successfully")
+	return db
+}
